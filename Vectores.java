3dcf0d356f0494e293c47d8a6601c88a9b2e7936
mcasta66 <31929652+mcasta66@users.noreply.github.com>
2025-06-16 17:40:34 +0200

    Add files via upload
    Exemple amb vectors, prova pel Google Sites

---
+++ Vectores.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Vectores {
+
+	final static int N = 10;
+
+	public static void leer(int[] vector) {
+//Leer vector
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce " + N + " números");
+
+		for (int i = 0; i < vector.length; i++) {
+			vector[i] = sc.nextInt();
+		}
+	}
+
+	public static void main(String[] args) {
+
+		int i;
+		int v[], v2[];
+		v = new int[10];
+		v2 = new int[5];
+
+		leer(v);
+		// Calcular suma
+		int suma = 0;
+		for (i = 0; i < N; i++) {
+			suma = suma + v[i];
+		}
+		System.out.println("La suma es " + suma);
+
+		leer(v2);
+		System.out.println("Adéu!");
+	}
+}
